05746742960e9c645bc2a3a366941c7b261c7cb3
eshalarch <eshalarch@gmail.com>
2026-05-19 22:04:35 +0530

    supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,11 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Environment variables (Vite/Hosting par yahi use karna hai)
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-  console.error("Supabase environment variables missing!");
-}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
