cb1b059c5b4152363fc6b709a5256c9fafc55656
eshalarch <eshalarch@gmail.com>
2026-05-20 15:13:46 +0530

    supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+// Vercel aur local environment ke liye variable set up
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://rkxwxkzqytzaajgrmloz.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || 'sb_publishable_baTDFIGYNPVWdkI9G78d9A_b34Xvnty';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
